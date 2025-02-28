a44a9dc1d15d7482f2f0d666ab56af2a81063434
igtechso-ayush <mohit.igtechso@gmail.com>
2025-02-28 12:03:40 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const db = require('./src/config/db.js');
 const authRoutes = require("./src/routers/auth.route.js")
 const messageRoutes = require('./src/routers/message.route.js')
 const initializeSocket = require("./src/websocket/socket.js")
-
 const app = express();
 const server = http.createServer(app);
 
@@ -25,7 +24,9 @@ app.use(morgan('dev'))
 db.connect();
 const PORT = process.env.PORT || 5000;
 
-
+app.use("/",(req,res)=>{
+    res.send("server is running")
+})
 // Initialize Socket.io
 const io = initializeSocket(server);
 // app.use((req, res, next) => {
@@ -38,5 +39,6 @@ app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
 
 server.listen(PORT, () => {
-    console.log("Server running on http://localhost:3000");
+    console.log("Server running ");
+    // console.log("Server running on http://localhost:3000");
 })
