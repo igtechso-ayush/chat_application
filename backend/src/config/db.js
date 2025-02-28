989d80e6e879c09eac5782b92c5b799122c89c14
igtechso-ayush <mohit.igtechso@gmail.com>
2025-02-28 12:16:19 +0530

    mongo config for deoploy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connect = () => {
     // mongoose.connect('mongodb://localhost:27017/Eco-practice')
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI,{
+        tls: true, // Ensures TLS connection
+        tlsAllowInvalidCertificates: false, // Avoid insecure connections
+    })
 
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
