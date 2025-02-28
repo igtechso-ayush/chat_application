692c0b0af725360673438c4eefcd7c3d61527d63
igtechso-ayush <mohit.igtechso@gmail.com>
2025-02-28 16:39:52 +0530

    change is path name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
-import Login from './pages/login'
+import Login from './pages/Login'
 import Chat from "./pages/Chat";
 import Rooms from "./pages/Rooms";
 import ErrorBoundary from "./pages/ErrorBoundary";
